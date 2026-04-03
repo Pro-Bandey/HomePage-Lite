fdcb80567a73fc5bc9300aed0bf60e11b82107f0
Pro Bandey <195909043+Pro-Bandey@users.noreply.github.com>
2026-04-03 15:14:34 +0500

    ok goog to you

--- js/popup.js
+++ js/popup.js
@@ -19,27 +19,27 @@ document.getElementById("openSidebar").addEventListener("click", async () => {
 
 // FireFox------------------
 
-const extInstaUninsta = typeof browser !== "undefined" ? browser : chrome;
-document.getElementById("openSidebar").addEventListener("click", async () => {
-    if (extInstaUninsta.sidebarAction && extInstaUninsta.sidebarAction.open) {
-        try {
-            await extInstaUninsta.sidebarAction.open();
-            window.close();
-        } catch (err) {
-            console.error("HomePage Lite: Failed to open Firefox sidebar", err);
-        }
-    }
-    else if (extInstaUninsta.sidePanel) {
-        try {
-            const [tab] = await extInstaUninsta.tabs.query({ active: true, currentWindow: true });
-            if (tab) {
-                await extInstaUninsta.sidePanel.open({ windowId: tab.windowId });
-                window.close();
-            }
-        } catch (err) {
-            console.error("HomePage Lite: Failed to open Chrome side panel", err);
-        }
-    } else {
-        alert("Sidebar functionality is not supported in this browser.");
-    }
-});
+// const extInstaUninsta = typeof browser !== "undefined" ? browser : chrome;
+// document.getElementById("openSidebar").addEventListener("click", async () => {
+//     if (extInstaUninsta.sidebarAction && extInstaUninsta.sidebarAction.open) {
+//         try {
+//             await extInstaUninsta.sidebarAction.open();
+//             window.close();
+//         } catch (err) {
+//             console.error("HomePage Lite: Failed to open Firefox sidebar", err);
+//         }
+//     }
+//     else if (extInstaUninsta.sidePanel) {
+//         try {
+//             const [tab] = await extInstaUninsta.tabs.query({ active: true, currentWindow: true });
+//             if (tab) {
+//                 await extInstaUninsta.sidePanel.open({ windowId: tab.windowId });
+//                 window.close();
+//             }
+//         } catch (err) {
+//             console.error("HomePage Lite: Failed to open Chrome side panel", err);
+//         }
+//     } else {
+//         alert("Sidebar functionality is not supported in this browser.");
+//     }
+// });
